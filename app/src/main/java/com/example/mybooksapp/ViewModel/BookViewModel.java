e26c007c674a3ce9238b3e91a7c910d689ef86c4
thanmaisai-asc <thanmai.sai@ascendion.com>
2024-08-10 21:08:26 +0530

    create ViewModal class + fixed dependencies

---
+++ app/src/main/java/com/example/mybooksapp/ViewModel/BookViewModel.java
@@ -0,0 +1,32 @@
+package com.example.mybooksapp.ViewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.mybooksapp.Model.Book;
+import com.example.mybooksapp.Repository.BookRepository;
+
+import java.util.List;
+
+public class BookViewModel extends AndroidViewModel {
+
+    private BookRepository bookRepository;
+    private LiveData<List<Book>> getAllBooks;
+
+    public BookViewModel(@NonNull Application application) {
+        super(application);
+        bookRepository=new BookRepository(application);
+        getAllBooks = bookRepository.getAllBooks();
+    }
+
+    public void insert(List<Book> bookList){
+        bookRepository.insert(bookList);
+    }
+
+    public LiveData<List<Book>> getGetAllBooks(){
+        return  getAllBooks;
+    }
+}
